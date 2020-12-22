35080423bf807a764c09298d71aba0cae98f4d0a
yzmu <sp35000@yahoo.com.br>
2020-12-22 13:42:04 -0300

    feat: MyToken created

---
+++ contracts/MyToken.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.6.1;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Gold", "GLD") public {
+        _mint(msg.sender, initialSupply);
+    }
+}
