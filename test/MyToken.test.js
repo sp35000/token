c22759b91f8dda78b07f5eb0bb1c2ebf301164fc
yzmu <sp35000@yahoo.com.br>
2020-12-23 16:10:06 -0300

    test: implementing MyTokenTest.js

---
+++ test/MyToken.test.js
@@ -0,0 +1,12 @@
+const MyToken = artifacts.require("MyToken");
+var chai = require("chai");
+const BN = web3.utils.BN;
+const chaiBN = require("chai-bn")(BN);
+chai.use(chaiBN);
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+contract("Token Test", async (accounts) => {
+    
+})
