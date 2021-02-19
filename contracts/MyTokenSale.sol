225d66b02592ee7245216e98f95550124d9c5b7d
yzmu <sp35000@yahoo.com.br>
2021-02-19 13:33:56 -0300

    feat: adding KYC - this version has this error: KycContract has no network configuration for its current network id

--- contracts/MyTokenSale.sol
+++ contracts/MyTokenSale.sol
@@ -1,19 +1,22 @@
 pragma solidity ^0.6.2;
 
 import "./Crowdsale.sol";
+import "./KycContract.sol";
 
 contract MyTokenSale is Crowdsale {
-
-// KycContract kyc;
-
+ KycContract kyc;
  constructor( uint256 rate, // rate in TKNbits
   address payable wallet,
-  IERC20 token)
- 
+  IERC20 token,
+  KycContract _kyc)
+
  Crowdsale(rate, wallet, token)
  public
  {
- 
+   kyc = _kyc;
  }
-
+ function _preValidatePurchase(address beneficiary, uint256 weiAmmount) internal view override {
+  super._preValidatePurchase(beneficiary,weiAmmount);
+  require(kyc.kycCompleted(beneficiary),"KYC not completed yet, aborting");
+ }
 }
--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -1,5 +1,6 @@
 const Token = artifacts.require("MyToken");
 const TokenSale = artifacts.require("MyTokenSale");
+const KycContract = artifacts.require("KycContract");
 
 var chai = require("./chaisetup.js");
 const BN = web3.utils.BN;
@@ -7,10 +8,30 @@ const expect = chai.expect;
 
 contract("TokenSale", async function(accounts) {
  const [ initialHolder, recipient, anotherAccount ] = accounts;
- 
+
  it("there shouldnt be any coins in my account", async() => {
   let instance = await Token.deployed();
   return expect(instance.balanceOf.call(initialHolder)).to.eventually.be.a.bignumber.equal(new BN(0));
+ });
+
+ it("all coins should be in the token sale contract", async() => {
+   let instance = await Token.deployed();
+  let balance = await instance.balanceOf.call(TokenSale.address);
+  let totalSupply = await instance.totalSupply.call();
+  return expect(balance).to.be.a.bignumber.equal(totalSupply);
+ });
+
+ it("should be possible to buy one token by simply sending ether to the smart contract", async() => {
+   let tokenInstance = await Token.deployed();
+   let tokenSaleInstance = await TokenSale.deployed();
+   let balanceBeforeAccount = await tokenInstance.balanceOf.call(recipient);
+
+   expect(tokenSaleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1","wei")})).to.be.rejected;
+
+   let kycInstance = await KycContract.deployed();
+   await kycInstance.setKycCompleted(recipient);
+   expect(tokenSaleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1","wei")})).to.be.fulfilled;
+   return expect(balanceBeforeAccount + 1).to.be.a.bignumber.equal(await tokenInstance.balanceOf.call(recipient));
  });
 
 });
